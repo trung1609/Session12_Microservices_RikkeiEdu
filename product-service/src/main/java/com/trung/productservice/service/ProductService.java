5020779d5acb181bb70738f6211a9504859d9aba
trung1609 <vmt162005@gmail.com>
2026-05-15 08:32:36 +0700

    update bt1

---
+++ product-service/src/main/java/com/trung/productservice/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.trung.productservice.service;
+
+import com.trung.productservice.dto.ProductRequestDTO;
+import com.trung.productservice.dto.ProductResponseDTO;
+import com.trung.productservice.exception.ResourceNotFoundException;
+
+import java.util.List;
+
+public interface ProductService {
+    ProductResponseDTO createProduct(ProductRequestDTO requestDTO);
+    ProductResponseDTO getProductById(Long id) throws ResourceNotFoundException;
+    List<ProductResponseDTO> getAllProducts();
+    void reduceStock(Long productId, Integer quantity) throws ResourceNotFoundException;
+    ProductResponseDTO updateProduct(Long id, ProductRequestDTO requestDTO) throws ResourceNotFoundException;
+}
---
+++ product-service/src/main/java/com/trung/productservice/service/impl/ProductServiceImpl.java
@@ -0,0 +1,71 @@
+package com.trung.productservice.service.impl;
+
+import com.trung.productservice.dto.ProductRequestDTO;
+import com.trung.productservice.dto.ProductResponseDTO;
+import com.trung.productservice.entity.Product;
+import com.trung.productservice.exception.ResourceNotFoundException;
+import com.trung.productservice.mapper.ProductMapper;
+import com.trung.productservice.repository.ProductRepository;
+import com.trung.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    @Override
+    public ProductResponseDTO createProduct(ProductRequestDTO requestDTO) {
+        Product product = ProductMapper.toEntity(requestDTO);
+        productRepository.save(product);
+
+        return ProductMapper.toDTO(product);
+    }
+
+    @Override
+    @Cacheable(value = "products", key = "#id")
+    public ProductResponseDTO getProductById(Long id) throws ResourceNotFoundException {
+        return productRepository.findById(id)
+                .map(ProductMapper::toDTO)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found with id: " + id));
+    }
+
+    @Override
+    public List<ProductResponseDTO> getAllProducts() {
+        return productRepository.findAll().stream()
+                .map(ProductMapper::toDTO)
+                .toList();
+    }
+
+    @Override
+    public void reduceStock(Long productId, Integer quantity) throws ResourceNotFoundException {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found with id: " + productId));
+
+        if (product.getStockQuantity() < quantity) {
+            throw new IllegalArgumentException("Insufficient stock for product id: " + productId);
+        }
+
+        product.setStockQuantity(product.getStockQuantity() - quantity);
+        productRepository.save(product);
+    }
+
+    @Override
+    @CacheEvict(value = "products", key = "#id")
+    public ProductResponseDTO updateProduct(Long id, ProductRequestDTO requestDTO) throws ResourceNotFoundException {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found with id: " + id));
+        product.setName(requestDTO.getName());
+        product.setPrice(requestDTO.getPrice());
+        product.setStockQuantity(requestDTO.getStockQuantity());
+        productRepository.save(product);
+        return ProductMapper.toDTO(product);
+    }
+
+
+}
